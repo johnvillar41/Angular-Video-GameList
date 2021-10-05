4384fd13f64d9d533c809ae146846d8c120584f6
johnvillar41 <johndanielvillar123@gmail.com>
2021-10-05 19:53:49 +0800

    Updated Home component

--- ng-video-game-db/src/app/components/home/home.component.ts
+++ ng-video-game-db/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { APIResponse, Game } from 'src/app/models';
 import { HttpService } from 'src/app/services/http.service';
 
@@ -8,16 +9,31 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public sort: string = "";
   public games: Array<Game> = [];
 
-  constructor(private httpService: HttpService, private activatedRoute: ActivatedRoute) {
+  private routeSub: Subscription = new Subscription;
+  private gameSub: Subscription = new Subscription;
 
+  constructor(
+    private httpService: HttpService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router) {
+
+  }
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
   }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params: Params) => {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
       if (params['game-searched']) {
         this.searchGames('metacrit', params['game-search']);
       } else {
@@ -27,10 +43,14 @@ export class HomeComponent implements OnInit {
   }
 
   searchGames(sort: string, search?: string) {
-    this.httpService.getGamesList(sort, search).subscribe((gameList: APIResponse<Game>) => {
+    this.gameSub = this.httpService.getGamesList(sort, search).subscribe((gameList: APIResponse<Game>) => {
       this.games = gameList.results;
       console.log(this.games);
     });
+  }
+
+  openGameDetails(id: string): void {
+    this.router.navigate(['details', id]);
   }
 
 }

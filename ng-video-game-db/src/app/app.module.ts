31d44cad0913fb5c128506c5f2396f47410affb0
johnvillar41 <johndanielvillar123@gmail.com>
2021-10-06 09:11:43 +0800

    Fixed appmodule

--- ng-video-game-db/src/app/app.module.ts
+++ ng-video-game-db/src/app/app.module.ts
@@ -24,12 +24,12 @@ import { DetailsComponent } from './components/details/details.component';
     AppComponent,
     SearchBarComponent,
     HomeComponent,
-    DetailsComponent
+    DetailsComponent,
   ],
   imports: [
     FormsModule,
     BrowserModule,
-    GaugeModule,
+    GaugeModule.forRoot(),
     MatIconModule,
     MatFormFieldModule,
     MatSelectModule,
--- ng-video-game-db/src/app/components/details/details.component.ts
+++ ng-video-game-db/src/app/components/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Game } from 'src/app/models';
@@ -9,7 +9,7 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
   public gameRating: number = 0;
   public gameId: string = "";
   public game!: Game;
@@ -21,6 +21,16 @@ export class DetailsComponent implements OnInit {
     private ActivatedRoute: ActivatedRoute,
     private httpService: HttpService
   ) { }
+
+  ngOnDestroy(): void {
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+  }
 
   ngOnInit(): void {
     this.routeSub = this.ActivatedRoute.params.subscribe((params: Params) => {
